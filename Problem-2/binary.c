b7ef301fd7ed6f001af5c98d9c71206247d8b8ce
GrantWishes <grant-gertsen@uiowa.edu>
2017-01-27 17:44:15 -0600

    Done except for maybe some small tweaks

--- Problem-2/binary.c
+++ Problem-2/binary.c
@@ -3,6 +3,7 @@
 
 int main(int argc, char *argv[]) {
 
+	/* Checking arguments and the such */
 	if(argc != 3) {
 		fprintf(stderr, "Incorrect amount of arguments\n");
 		exit(1);
@@ -22,25 +23,29 @@ int main(int argc, char *argv[]) {
 	unsigned char buffer;
 	int amount;
 	int size;
+	/* Why didn't I think of this when I was doing part one??? */
+	char hexchars[] = "0123456789ABCDEF";
+	
+	fread(&amount, sizeof(int), 1, inFile); // read in how many numbers
 
-	fread(&amount, sizeof(int), 1, inFile);
-
-	printf("Buffer test: %d\n", amount);
+	//printf("Buffer test: %d\n", amount);
 	fprintf(outFile, "%d\n", amount);
 
-//	for(int i = 0; i < amount; i++) {
+	
+	for(int i = 0; i < amount; i++) {
+		// How many bytes it will be
 		fread(&size, sizeof(int), 1, inFile);
-		printf("Length in bytes is %d\n", size);
-		fprintf(outFile, "%d", (2 * size) + 2);
-		//buffer = malloc();
-		for(int j = 0; j < (size / 4); j++) {
+		//printf("Length in bytes is %d\n", size);
+		fprintf(outFile, "%d 0X", (2 * size) + 2);
+		for(int j = 0; j < size ; j++) {
+			// Read in a byte
 			fread(&buffer, 1, 1, inFile);
-			printf("Buffer value: %u\n", buffer);
+			// Convert to hex
+			fprintf(outFile, "%c", hexchars[buffer/16]);
+			fprintf(outFile, "%c", hexchars[buffer%16]);			
 		}
-//	}
+		fprintf(outFile, "\n");
+	}
 	
-	
-	
-
 	return 0;
 }
